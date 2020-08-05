b386cee2d963eed1662a796753c9c14cc656a5b8
JorgeYakushi <42720518+JorgeYakushi@users.noreply.github.com>
2020-08-05 02:10:18 -0500

    Add files via upload

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,11 @@
 const navButton = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav__list__container");
 navButton.addEventListener("click", () => {
-  navButton.querySelector("span").classList.toggle("is-active");
+  toggleMenu();
 });
+
+const toggleMenu = () => {
+  navButton.querySelector("span").classList.toggle("is-active");
+  navList.classList.toggle("nav__list--passive");
+  navList.classList.toggle("nav__list--active");
+};
